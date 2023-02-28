2196c737a8d5ff3ee64653d305843b60db39da39
yago <askyago@hotmail.com>
2023-02-28 01:23:47 -0400

    Initial Project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Todo from "./components/Todo.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h2>To-do List!!</h2>
+    <Todo />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { v4 as uuidv4 } from "uuid";
+import { ref } from "vue";
+
+interface Todo {
+    id: string;
+    todo: string;
+}
+
+const todos = ref<Todo[]>([]);
+const todo  = ref("");
+
+
+const add = () => {
+    todos.value.push({
+        id: uuidv4(),
+        todo: todo.value
+    })
+}
+
+function test<T>(a: T){}
+test<string>("")
+
+const del = (index: number) => {
+    todos.value.splice(index, 1);
+}
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="add">
+            <input v-model="todo" />
+            <button type="submit">Add</button>
+        </form>
+        <div 
+        v-for="(todo, index) of todos"
+        :key="todo.id"
+        >
+            {{ todo.todo }}
+            <button @click="del(index)">delete</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
